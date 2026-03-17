7cd16599cc581e03a47fb23425f45be708c2d97c
Dr. J. Senthil Kumar <35796605+senvimjag@users.noreply.github.com>
2026-03-17 15:44:16 +0530

    Implement Writing and Reading from a file using System Calls
    Implement file writing and reading using system calls.

---
+++ 13_filewriteread.c
@@ -0,0 +1,15 @@
+//Writing and Reading from a file using System Calls
+#include<stdio.h>
+#include<fcntl.h> //File Control System call functions
+#include<unistd.h>
+int main(){
+     int fd; //File discriptor
+     char buf[50];
+     fd = open("myfile.txt", O_CREAT | O_RDWR);
+     write(fd,"Welcome of File handling Emb. Linux",36);
+     lseek(fd, 0, SEEK_SET);
+     read(fd,buf,36);
+     printf("\nFile Contents:%s",buf);
+     close(fd);
+     return 0;
+}
