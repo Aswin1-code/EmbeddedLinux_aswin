94a6b446f532310f9528f8fe339919956781d0d6
Aswin1-code <500aswin@gmail.com>
2026-04-09 03:30:03 +0530

    updated

--- 18_socketclient.c
+++ 18_socketclient.c
@@ -6,15 +6,25 @@
 #define PORT 8080
 int main(){
     int soc;
+    
     struct sockaddr_in add;
+
     char *msg = "Hello from Client";
+    
     soc = socket(AF_INET, SOCK_STREAM, 0); //Create socket
+    
     add.sin_family = AF_INET;
+    
     add.sin_port = htons(PORT);
+    
     inet_pton(AF_INET, "127.0.0.1", &add.sin_addr); //Localhost address
+    
     connect(soc, (struct sockaddr *)&add, sizeof(add)); //Connect server
+    
     send(soc, msg, strlen(msg), 0); //Send message
+    
     printf("Message sent to server\n");
+    
     close(soc); //Close socket
     return 0;
 }
