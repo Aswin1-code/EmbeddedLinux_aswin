48a3e43f8aeb29479fc4adc8e313e9e5ed113665
Aswin1-code <500aswin@gmail.com>
2026-04-08 21:55:56 +0530

    updated

--- 17_sigusr.c
+++ 17_sigusr.c
@@ -5,29 +5,29 @@
 #include <unistd.h>
 #include <sys/wait.h>
 void handler(int sig) { //Signal handler function
-    if (sig == SIGUSR1) {
+    if (sig == SIGUSR1) {  //checking signal type
         printf("Child (PID %d): Received SIGUSR1 from parent!\n", getpid());
         exit(0); // Terminate after handling
     }
 }
 int main() {
     pid_t pid;
-    struct sigaction sa;
+    struct sigaction sa; //struct sigaction - is defined in signal.h
     pid = fork();
     if (pid == 0) {
         printf("Child (PID %d): Waiting for signal...\n", getpid());
-        sa.sa_handler = handler; //Assign Signal handler function to action
-        if (sigaction(SIGUSR1, &sa, NULL) == -1) {
+        sa.sa_handler = handler; //Assigning Signal handler function
+        if (sigaction(SIGUSR1, &sa, NULL) == -1) { // sigaction -> register handler
             perror("sigaction");
             exit(1);
         }
-        while (1) {
+        while (1) {   // waiting for signal
             sleep(1);
         }
     } else {
         sleep(2); // Parent ensuring child is ready
         printf("Parent (PID %d): sending SIGUSR1 to child (PID %d)\n", getpid(), pid);
-        kill(pid, SIGUSR1);
+        kill(pid, SIGUSR1); //send signal to child
         wait(NULL); //Parent waiting for child to complete
         printf("Parent: Child finished, exiting.\n");
     }
